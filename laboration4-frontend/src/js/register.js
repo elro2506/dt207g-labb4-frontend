aa22d8bb5f7949ba040af82c5f52bedf43934421
Elin Ronda <elro2506@student.miun.se>
2026-05-08 11:33:31 +0200

    Startat upp frontendprojektet och strukturerat upp mappar och filer

---
+++ laboration4-frontend/src/js/register.js
@@ -0,0 +1,30 @@
+const form = document.getElementById("register");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        const response = await fetch(
+            "https://dt207-labb4-backend.onrender.com/api/register",
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    username,
+                    password
+                })
+            }
+        );
+
+        const data = await response.json();
+        console.log(data);
+        alert("Användare skapad!");
+    } catch (error) {
+        console.log(error);
+    }
+});
