1a5ca6a51a15ed78dd30c62bfed1be2091ff5a38
Elin Ronda <elro2506@student.miun.se>
2026-05-10 15:31:57 +0200

    Ändrat från localstorage till sessionstorage för att testa om det åtgärdar servererror vid inloggning

--- laboration4-frontend/src/js/checkAuth.js
+++ laboration4-frontend/src/js/checkAuth.js
@@ -1,6 +1,6 @@
 //Kollar om det finns en token - annars skickas man till inloggningssidan
 "use strict";
 
-if(!localStorage.getItem("website_token")) {
+if(!sessionStorage.getItem("website_token")) {
     window.location.href ="login.html";
 }
